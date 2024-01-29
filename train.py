44a4223cc2c9a74b2df4ffa8973c2fccebed95e8
wslDavid <dav1dch@outlook.com>
2024-01-29 16:54:09 +0800

    model training

---
+++ train.py
@@ -0,0 +1,23 @@
+from model import DBA
+from generates.data_utils import make_dataloader
+import torch
+
+
+def train():
+    model = DBA().cuda()
+
+    for name, param in model.named_parameters():
+        if "superpoint" in name:
+            param.requires_grad = False
+    dataloaders = make_dataloader(None)
+
+    for index1, index2, image1, image2, depth1, depth2, pose1, pose2 in dataloaders[
+        "train"
+    ]:
+        deltaPose = torch.matmul(torch.linalg.inv(pose1), pose2)
+        R_true = torch.tensor(deltaPose[..., :3, :3])
+        t_true = torch.tensor(deltaPose[..., :3, 3:].transpose(1, 2))
+
+
+if __name__ == "__main__":
+    train()
